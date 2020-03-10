54c3a0df985a01c29749a2dcdad37e2979842025
a1015493089 <1015493089@qq.com>
2020-03-10 21:56:55 +0800

    session

--- src/main/java/com/example/demobs/controller/AuthorizeController.java
+++ src/main/java/com/example/demobs/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 
 @Controller
@@ -23,7 +25,8 @@ public class AuthorizeController {
     private String redirect_url;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                            HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
@@ -32,7 +35,13 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirect_url);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            //登入成功
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        } else {
+            //登入失败 重新登入
+            return "redirect:/";
+        }
     }
 }
