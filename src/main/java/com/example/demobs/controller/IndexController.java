87346c3bd2933e4b48b9c5d6943b70d0d2a84812
a1015493089 <1015493089@qq.com>
2020-03-13 04:03:27 +0800

    长久登录态

--- src/main/java/com/example/demobs/controller/IndexController.java
+++ src/main/java/com/example/demobs/controller/IndexController.java
@@ -1,12 +1,33 @@
 package com.example.demobs.controller;
 
+import com.example.demobs.mapper.UserMapper;
+import com.example.demobs.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public  String index() {
+    public  String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null){              //登入态检验
+            for (Cookie cookie : cookies) {
+                if(cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user=userMapper.findByToken(token);
+                    if(user!=null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                }
+            }
+        }
         return "index";
     }
 }
--- src/main/java/com/example/demobs/mapper/UserMapper.java
+++ src/main/java/com/example/demobs/mapper/UserMapper.java
@@ -3,10 +3,13 @@ package com.example.demobs.mapper;
 import com.example.demobs.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper{
     @Insert("INSERT INTO user(name,account_id,token,gmt_creat,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreat},#{gmtModified})")
     void insert(User user);
-
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
