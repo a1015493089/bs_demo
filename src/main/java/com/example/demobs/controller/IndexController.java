a7479013769ac809ec6e8c621b3241461fb73b86
a1015493089 <1015493089@qq.com>
2020-03-17 04:41:07 +0800

    问题列表

--- src/main/java/com/example/demobs/controller/IndexController.java
+++ src/main/java/com/example/demobs/controller/IndexController.java
@@ -1,20 +1,29 @@
 package com.example.demobs.controller;
 
+import com.example.demobs.dto.QuestionDTO;
+import com.example.demobs.mapper.QuestionMapper;
 import com.example.demobs.mapper.UserMapper;
+import com.example.demobs.model.Question;
 import com.example.demobs.model.User;
+import com.example.demobs.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("/")
-    public  String index(HttpServletRequest request) {
+    public  String index(HttpServletRequest request,
+                         Model model) {
         Cookie[] cookies = request.getCookies();
         if(cookies!=null&&cookies.length!=0){              //登入态检验
             for (Cookie cookie : cookies) {
@@ -27,6 +36,8 @@ public class IndexController {
                 }
             }
         }
+        List<QuestionDTO> questionsList=questionService.list();
+        model.addAttribute("questions",questionsList);
         return "index";
     }
 }
--- src/main/java/com/example/demobs/mapper/QuestionMapper.java
+++ src/main/java/com/example/demobs/mapper/QuestionMapper.java
@@ -3,9 +3,14 @@ package com.example.demobs.mapper;
 import com.example.demobs.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface QuestionMapper {
     @Insert("insert into question(id,title,description,gmt_creat,gmt_modified,creator,tag) values(#{id},#{title},#{description},#{gmtCreat},#{gmtModified},#{creator},#{tag})")
     void creat(Question question);
+    @Select("select * from question")
+    List<Question> list();
 }
