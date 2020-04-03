8359ad0089c2278698d24fffa337a2307fa0766c
a1015493089 <1015493089@qq.com>
2020-04-03 16:52:39 +0800

    拦截器

--- src/main/java/com/example/demobs/controller/IndexController.java
+++ src/main/java/com/example/demobs/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.example.demobs.controller;
 
 import com.example.demobs.dto.PaginationDTO;
 import com.example.demobs.mapper.UserMapper;
-import com.example.demobs.model.User;
 import com.example.demobs.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,13 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
-    @Autowired
-    private UserMapper userMapper;
     @Autowired
     private QuestionService questionService;
     @GetMapping("/")
@@ -24,18 +20,6 @@ public class IndexController {
                          Model model,
                          @RequestParam(name = "page",defaultValue = "1") Integer page,
                          @RequestParam(name = "size",defaultValue = "2")Integer size) {
-        Cookie[] cookies = request.getCookies();
-        if(cookies!=null&&cookies.length!=0){              //登入态检验
-            for (Cookie cookie : cookies) {
-                if(cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                    User user=userMapper.findByToken(token);
-                    if(user!=null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                }
-            }
-        }
         PaginationDTO pagination=questionService.list(page,size);
         model.addAttribute("pagination",pagination);
         return "index";
--- src/main/java/com/example/demobs/controller/PublishController.java
+++ src/main/java/com/example/demobs/controller/PublishController.java
@@ -1,7 +1,6 @@
 package com.example.demobs.controller;
 
 import com.example.demobs.mapper.QuestionMapper;
-import com.example.demobs.mapper.UserMapper;
 import com.example.demobs.model.Question;
 import com.example.demobs.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @Autowired
     private QuestionMapper questionMapper;
-
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish(){
@@ -48,21 +43,8 @@ public class PublishController {
             model.addAttribute("error","标签不能为空");
             return "publish";
         }
-
-
-        User user=null;
-        Cookie[] cookies = request.getCookies();
-        if(cookies!=null&&cookies.length!=0){              //登入态检验
-            for (Cookie cookie : cookies) {
-                if(cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                     user=userMapper.findByToken(token);
-                    if(user!=null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                }
-            }
-        }
+        //登入态检验
+        User user=(User)request.getSession().getAttribute("user");
         if(user==null) {
             model.addAttribute("error","用户未登入");
             return "publish";}
