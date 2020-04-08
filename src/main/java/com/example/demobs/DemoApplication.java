a0e774aad5c6a27bb70ada90baacf62a3c3dd505
a1015493089 <1015493089@qq.com>
2020-04-09 00:47:23 +0800

    BUG修复

--- src/main/java/com/example/demobs/DemoApplication.java
+++ src/main/java/com/example/demobs/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demobs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.demobs.mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
